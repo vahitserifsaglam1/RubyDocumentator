aea507330c982078fc075eee9eaae6e95a71153d
anonymframework <serif.saglam119@gmail.com>
2015-10-10 19:02:55 +0300

    made some updates

--- documentator.rb
+++ documentator.rb
@@ -97,7 +97,10 @@ class Documentator
     @finded_files.each do |file, index|
       name_without_extension = File.basename(file, '.*')
       name_with_extension = File.basename file
-      parser = Parser.new(name_without_extension, @page_options, file)
+
+      aliases = @aliases.include?(name_with_extension) ? @aliases[name_with_extension] : nil
+
+      parser = Parser.new(name_without_extension, @page_options, file, aliases)
       parsed[index] = parser.parse
     end
 
--- parser.rb
+++ parser.rb
@@ -1,15 +1,16 @@
 class Parser
 
-  attr_accessor :name_without_extension, :page_options, :path
+  attr_accessor :name_without_extension, :page_options, :path, :aliases
 
   # create a new parser instance and register instance variables
   # @param [String] name
   # @param [Array] page_options
   # @param [String] path
-  def initialize name, page_options, path
+  def initialize name, page_options, path, aliases
     @name_without_extension = name
     @page_options = page_options
     @path = path
+    @aliases = aliases
   end
 
   #parse the file content
