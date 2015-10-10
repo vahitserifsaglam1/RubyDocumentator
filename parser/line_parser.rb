72b62d5bc45f27e244107838c63ca76af9f18b42
anonymframework <serif.saglam119@gmail.com>
2015-10-10 20:45:57 +0300

    made some updates

--- parser/line_parser.rb
+++ parser/line_parser.rb
@@ -6,6 +6,20 @@ class LineParser
     # parse the content and create html content
     # @param [String] content
     def parse_line content
+      @content = content
+      created_content = content
+
+      @@charecters.each do |charecter|
+         if finded = self.find_and_replace_charecter(charecter)
+            created_content = finded
+         elsif regex_findex = self.check_regex(charecter)
+           created_content = regex_findex
+         end
+      end
+    end
+
+    # if content contains this charecter we will find and replace it
+    def find_and_replace_charecter charecter
 
     end
   end
