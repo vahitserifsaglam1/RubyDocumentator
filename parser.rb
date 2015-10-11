bbdb090771179d601d0aba52d7d9d5743b70245c
anonymframework <serif.saglam119@gmail.com>
2015-10-11 10:44:31 +0300

    made some updates

--- parser.rb
+++ parser.rb
@@ -1,7 +1,7 @@
 require_relative 'parser/line_parser'
 class Parser
 
-  attr_accessor :name_without_extension, :page_options, :path, :aliases
+  attr_accessor :name_without_extension, :page_options, :path, :aliases, :headers
   attr_reader :parsed
 
   # create a new parser instance and register instance variables
@@ -9,6 +9,7 @@ class Parser
   # @param [Array] page_options
   # @param [String] path
   def initialize name, page_options, path, aliases
+    @@headers = []
     @name_without_extension = name
     @page_options = page_options
     @path = path
